1ce409ab5e38e1cca186515c3214fc3d7106bfc7
Miles Perry <mpperry@iastate.edu>
2018-09-04 14:40:33 -0500

    added install stuff

--- var/spack/repos/builtin/packages/karma/package.py
+++ var/spack/repos/builtin/packages/karma/package.py
@@ -22,38 +22,23 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install karma
-#
-# You can edit this file again by typing:
-#
-#     spack edit karma
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
 class Karma(Package):
     """FIXME: Put a proper description of your package here."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "http://www.example.com"
     url      = "ftp://ftp.atnf.csiro.au/pub/software/karma/karma-1.7.25-common.tar.bz2"
 
     version('1.7.25-common', sha256='afda682d79c0923df5a6c447a32b09294da1582933abae3205c008104da54fbd')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('libx11')
 
     def configure(self):
         'LD_LIBRARY_PATH'= self.spec['lib']
 
     def install(self, spec, prefix):
+        dst = join_path(prefix, 'Karma')
+
+        install('csh_script/install_karma', dst)

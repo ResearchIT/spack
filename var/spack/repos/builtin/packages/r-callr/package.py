257d178c28f65a5452c66ef7a5832985b69229ce
molecuul <jtst@iastate.edu>
2018-09-25 14:15:25 -0500

    r-callr: added versions, correct dependencies

--- var/spack/repos/builtin/packages/r-callr/package.py
+++ var/spack/repos/builtin/packages/r-callr/package.py
@@ -31,6 +31,18 @@ class RCallr(RPackage):
        does exactly that."""
 
     homepage = "https://github.com/MangoTheCat/callr"
-    url      = "https://cran.r-project.org/src/contrib/callr_1.0.0.tar.gz"
+    url      = "https://cran.r-project.org/src/contrib/callr_3.0.0.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/callr/callr_2.0.4.tar.gz"
 
-    version('1.0.0', 'd9af99bb95696310fa1e5d1cb7166c91')
+    version('3.0.0', sha256='e36361086c65660a6ecbbc09b5ecfcddee6b59caf75e983e48b21d3b8defabe7')
+    version('2.0.4', sha256='0e3fa4e047f61f4e29ab2dee8f585861fb77a5f5ad6c81f125e98b6130c6a380')
+    version('2.0.3', sha256='f1f7d511b215eb88d8680edef09d31eb93b3ef8beb124f8edf0c82601f7575cc')
+    version('2.0.2', sha256='778595e3f0b08f4e33a3103bd8e84a183945074f9e7404cdee8d72b7d3b8a154')
+    version('2.0.1', sha256='067bb1cda50a63df626a23128d1e32eb20a2ed3ea6cae0937bef97ce6dc6c3e6')
+    version('2.0.0', sha256='b89482fa11c02058e636a19e9286a30f4e2c299a7b1aa52d87cd839520b2a4bb')
+    version('1.0.0', sha256='2c56808c723aba2ea8a8b6bbdc9b8332c96f59b119079861dd52f5988c27f715')
+
+    depends_on('r-base64enc', type=('build', 'run'))
+    depends_on('r-processx', type=('build', 'run'))
+    depends_on('r-r6', type=('build', 'run'))
+    depends_on('r-utils', type=('build', 'run'))

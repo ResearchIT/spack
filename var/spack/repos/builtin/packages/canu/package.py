5bf0d1a5225d20c7a3725472f47149e69bcc8cb0
Miles Perry <mpperry@iastate.edu>
2018-12-11 15:22:39 -0600

    canu edits to fix can't find defaults.pm error

--- var/spack/repos/builtin/packages/canu/package.py
+++ var/spack/repos/builtin/packages/canu/package.py
@@ -2,7 +2,6 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
 from spack import *
 
 
@@ -13,15 +12,14 @@ class Canu(MakefilePackage):
     homepage = "http://canu.readthedocs.io/"
     url      = "https://github.com/marbl/canu/archive/v1.5.tar.gz"
 
-    version('1.7.1', sha256='c314659c929ee05fd413274f391463a93f19b8337eabb7ee5de1ecfc061caafa')
     version('1.5', '65df275baa28ecf11b15dfd7343361e3')
+    version('1.8', 'e35644ff35a5ca214b80f0491af964dc')
 
     depends_on('gnuplot', type='run')
     depends_on('jdk', type='run')
     depends_on('perl', type='run')
 
     build_directory = 'src'
-    build_targets = ['clean']
 
     def patch(self):
         # Use our perl, not whatever is in the environment
@@ -29,6 +27,22 @@ class Canu(MakefilePackage):
                     '#!{0}'.format(self.spec['perl'].command.path),
                     'src/pipelines/canu.pl')
 
+    def setup_environment(self, spack_env, run_env):
+        run_env.prepend_path('PERL5LIB', self.prefix.canu)
+        run_env.prepend_path('PERL5LIB', self.prefix.src.pipelines.canu)
+        run_env.prepend_path('LD_LIBRARY_PATH', self.prefix.src.pipelines.canu)
+
     def install(self, spec, prefix):
-        with working_dir(self.build_directory):
-            make('all', 'TARGET_DIR={0}'.format(prefix))
+        # replicate the Makefile logic here:
+        # https://github.com/marbl/canu/blob/master/src/Makefile#L344
+        uname = which('uname')
+        ostype = uname(output=str).strip()
+        machinetype = uname('-m', output=str).strip()
+        if machinetype == 'x86_64':
+            machinetype = 'amd64'
+        target_dir = '{0}-{1}'.format(ostype, machinetype)
+        bin = join_path(target_dir, 'bin')
+
+        install_tree(bin, prefix.bin)
+        install_tree('src', prefix.src)
+        install_tree('src/pipelines/canu', prefix.canu)

87d23fc6beccc8d5a4bd3597740ce990da242bc5
Levi Baber <baber@iastate.edu>
2019-02-04 10:48:25 -0600

    braker: new versions

--- var/spack/repos/builtin/packages/braker/package.py
+++ var/spack/repos/builtin/packages/braker/package.py
@@ -11,10 +11,14 @@ class Braker(Package):
        that combines the advantages of GeneMark-ET and AUGUSTUS"""
 
     homepage = "http://exon.gatech.edu/braker1.html"
-    url      = "http://bioinf.uni-greifswald.de/augustus/binaries/BRAKER_v2.1.0.tar.gz"
-    list_url = "http://bioinf.uni-greifswald.de/augustus/binaries/old"
+    url      = "https://github.com/Gaius-Augustus/BRAKER/archive/v2.1.2.tar.gz"
+    # Releases have moved to github
 
-    version('2.1.0', '5f974abcceb9f96a11668fa20a6f6a56')
+    version('2.1.2', sha256='9f178c5fe64ae358dcba9936802d24e330312e698a3f7930d1f91e58974129d3')
+    version('2.1.1', sha256='10674382431975928217875df7c6953f841c69934057137c0d177c9dbfad18af',
+            url='https://github.com/Gaius-Augustus/BRAKER/archive/v2.1.1-tag1.tar.gz')
+    version('2.1.0', '5f974abcceb9f96a11668fa20a6f6a56',
+            url='http://bioinf.uni-greifswald.de/augustus/binaries/old/BRAKER_v2.1.0.tar.gz')
     version('1.11', '297efe4cabdd239b710ac2c45d81f6a5',
             url='http://bioinf.uni-greifswald.de/augustus/binaries/old/BRAKER1_v1.11.tar.gz')
 
@@ -22,7 +26,7 @@ class Braker(Package):
     depends_on('perl-scalar-util-numeric', type=('build', 'run'))
     depends_on('perl-parallel-forkmanager', type=('build', 'run'))
     depends_on('perl-file-which', type=('build', 'run'))
-    depends_on('augustus@3.2.3')
+    depends_on('augustus@3.2.3:')
     depends_on('genemark-et')
     depends_on('bamtools')
     depends_on('samtools')
